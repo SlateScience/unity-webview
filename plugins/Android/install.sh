60e032ac0a7896be8405e6a7d010fa11f398560f
Jono Barel <jono.b@slatescience.com>
2025-02-13 16:40:14 +0000

    Update JAVA references to Unity 6 directory APPL-9524

--- plugins/Android/install.sh
+++ plugins/Android/install.sh
@@ -7,13 +7,13 @@ CWD=`cd $CWD && pwd -P`
 
 case $(uname) in
 Darwin)
-    export JAVA_HOME='/Applications/Unity/Hub/Editor/2022.3.32f1/PlaybackEngines/AndroidPlayer/OpenJDK'
-    export ANDROID_SDK_ROOT='/Applications/Unity/Hub/Editor/2022.3.32f1/PlaybackEngines/AndroidPlayer/SDK'
+    export JAVA_HOME='/Applications/Unity/Hub/Editor/6000.0.37f1/PlaybackEngines/AndroidPlayer/OpenJDK'
+    export ANDROID_SDK_ROOT='/Applications/Unity/Hub/Editor/6000.0.37f1/PlaybackEngines/AndroidPlayer/SDK'
     export PATH=$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/tools/bin:$JAVA_HOME/bin:$PATH
     ;;
 MINGW64_NT*)
-    export JAVA_HOME='/c/PROGRA~1/Unity/Hub/Editor/2022.3.32f1/Editor/Data/PlaybackEngines/AndroidPlayer/OpenJDK'
-    export ANDROID_SDK_ROOT='/c/PROGRA~1/Unity/Hub/Editor/2022.3.32f1/Editor/Data/PlaybackEngines/AndroidPlayer/SDK'
+    export JAVA_HOME='/c/PROGRA~1/Unity/Hub/Editor/6000.0.37f1/Editor/Data/PlaybackEngines/AndroidPlayer/OpenJDK'
+    export ANDROID_SDK_ROOT='/c/PROGRA~1/Unity/Hub/Editor/6000.0.37f1/Editor/Data/PlaybackEngines/AndroidPlayer/SDK'
     export PATH=$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/tools/bin:$JAVA_HOME/bin:$PATH
     ;;
 esac
@@ -22,14 +22,14 @@ DEST_DIR='../../build/Packager/Assets/Plugins/Android'
 if [ ! -d "$JAVA_HOME" ]
 then
     echo 'Looking for Java Home'
-    echo 'From Unity Hub, please install 2022.3.32f1 with the android module.'
+    echo 'From Unity Hub, please install 6000.0.37f1 with the android module.'
     exit 1
 fi
 
 # options
 TARGET="webview"
 MODE="Release"
-UNITY="2022.3.32f1"
+UNITY="6000.0.37f1"
 for OPT in $*
 do
     case $OPT in
